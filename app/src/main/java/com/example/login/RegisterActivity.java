d43c56a5180ff810e34fa7e926fcd3cb7d437aa9
MuhammadIlhamWibowo <Muhammad.Ilham.Wibowo@gmail.com>
2019-12-02 07:17:07 +0700

    instansiasi ui component

--- app/src/main/java/com/example/login/RegisterActivity.java
+++ app/src/main/java/com/example/login/RegisterActivity.java
@@ -3,12 +3,28 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button registrasi;
+    private EditText nama, email, tempatLahir, tanggalLahir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        nama = findViewById(R.id.register_etNama);
+        email = findViewById(R.id.register_etEmail);
+        tempatLahir = findViewById(R.id.register_etTempatLahir);
+        tanggalLahir = findViewById(R.id.register_etTanggalLahir);
+        registrasi = findViewById(R.id.btnRegister);
+
+        doRegister();
+    }
+
+    private void doRegister() {
     }
 }
